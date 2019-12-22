b6982703be654499805d9ec9e7438e69751155c8
Ahmed Muhammed Alaa <ahmedkellany51@gmail.com>
2019-12-22 07:57:24 +0200

    new

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './profile.dart';
-//import './the_main_page.dart';
+import './list_item.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -13,6 +13,8 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   String appBarTitle = 'الرئيسيه';
   String mainPage = 'الرئيسيه';
   String profilePage = 'حسابي';
+  String textFieldContent = '';
+  TextEditingController controller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -65,13 +67,16 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
           controller: _tabController,
           children: myTabs.map((Tab tab) {
             final String label = tab.text;
-            return mainPage == label ? _buildMainPage() : _buildProfile();
+            return mainPage == label
+                ? _buildMainPage(context)
+                : _buildProfile();
           }).toList(),
         ),
       ),
     );
   }
 
+// صفحة حسابي
   Widget _buildProfile() {
     IconData icon1 = Icons.star;
     IconData icon2 = Icons.settings;
@@ -105,12 +110,124 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     );
   }
 
-  Widget _buildMainPage() {
+// الرئيسيه
+  Widget _buildMainPage(BuildContext myContext) {
+    List<Card> cards = [];
+    AlertDialog blogDialog = AlertDialog(
+      content: buildDialog(),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 5.0),
+          child: Container(
+            color: Colors.green,
+            height: 30.0,
+            child: RaisedButton(
+              elevation: 5.0,
+              child: Text(
+                'نشر',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                setState(() {
+                  cards.add(buildListItem(textFieldContent));
+                  print('${cards.length} from el nashr');
+                });
+                Navigator.of(myContext).pop();
+                controller.clear();
+              },
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 60.0),
+          child: FlatButton(
+            child: Text('تجاهل'),
+            onPressed: () => Navigator.of(myContext).pop(),
+          ),
+        )
+      ],
+    );
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add,color: Colors.white,),
-        backgroundColor: Color(0xFF39796b),
-        onPressed: null,// implement the add posts code function.
+        floatingActionButton: FloatingActionButton(
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            backgroundColor: Color(0xFF39796b),
+            onPressed: () => showDialog(
+                context: myContext, builder: (myContext) => blogDialog)),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                    itemCount: cards.length,
+                    itemBuilder: (myContext, int Index) {
+                      return ListTile(
+                        title: cards[Index],
+                      );
+                    }),
+              ),
+            ],
+          ),
+        )
+        );
+  }
+
+  Widget buildDialog() {
+    return Container(
+      height: 300.0,
+      child: Column(
+        children: <Widget>[
+          Container(
+              color: Colors.black12,
+              height: 150.0,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.photo_camera,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    'Upload Photo',
+                    style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                  )
+                ],
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  'اكتب تعليقا حول الصوره',
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(color: Colors.grey, fontSize: 14.0),
+                ),
+              )
+            ],
+          ),
+          //Text('اكتب تعليقا حول الصوره'ئ, textDirection: TextDirection.rtl,),
+          Expanded(
+            child: TextField(
+              textDirection: TextDirection.rtl,
+              controller: controller,
+              maxLines: 3,
+              maxLength: 200,
+              autocorrect: true,
+              autofocus: true,
+              cursorColor: Colors.black26,
+              onSubmitted: (val) {
+                setState(() {
+                  textFieldContent = val;
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/list_item.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+Card buildListItem(String commentText) {
+  return Card(
+    child: Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.favorite),
+                    onPressed: null,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.bookmark),
+                    onPressed: null,
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.share),
+                    onPressed: null,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 2.0, left: 50.0),
+                child: Text(
+                  'اسم المستخدم',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(80.0),
+                    border: Border.all(width: 2.0)),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          color: Colors.grey,
+          width: double.infinity,
+          height: 200.0,
+          child: Image.asset('assets/images/ahmed.jpg'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                commentText,
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.right,
+                overflow: TextOverflow.visible,
+                maxLines: 4,
+                style: TextStyle(fontWeight: FontWeight.w300),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
